2a0a22cb393aa00bd90934e3feeffdb3b1499646
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2023-07-05 15:28:14 +0530

    Create CountSubarrayswithGivenXOR.java

---
+++ CountSubarrayswithGivenXOR.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public static int subarraysXor(ArrayList<Integer> arr, int x) {
+		int n = arr.size();
+		int xr = 0;
+
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(xr, 1);
+
+		int cnt = 0;
+
+		for(int i=0; i<n; i++) {
+			xr = xr ^ arr.get(i);
+
+			int a = xr^x;    //By formula : x = xr^k
+
+			if(map.containsKey(a)) {
+				cnt += map.get(a);
+			}
+
+			if(map.containsKey(xr)) {
+				map.put(xr, map.get(xr) + 1);
+			}
+			else {
+				map.put(xr, 1);
+			}
+		}
+		return cnt;
+	}
+}
