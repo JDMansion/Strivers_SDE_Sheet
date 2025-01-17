e013aa38f504ae367e69ec0fcac310935ebc7c64
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-01-17 15:33:00 +0530

    Update numberOfOccurances.cpp

--- Binary Search/numberOfOccurances.cpp
+++ Binary Search/numberOfOccurances.cpp
@@ -1,5 +1,78 @@
 // Number of Occurances   ( GFG )
 
+//Optimal Solution 
+//Binary Search
+class Solution {
+  public:
+  
+    int firstOccurance(vector<int>& arr, int target) {
+        int start = 0;
+        int end = arr.size() - 1;
+        int ans = -1;
+        
+        if(target < arr[start]) {
+            return ans;
+        }
+        
+        while(start <= end) { 
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] == target) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else if(target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+    
+    int lastOccurance(vector<int>& arr, int target) {
+        int start = 0;
+        int end = arr.size() - 1;
+        int ans = -1;
+        
+        if(target > arr[end]) {
+            return ans;
+        }
+        
+        while(start <= end) { 
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] == target) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else if(target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+  
+    int countFreq(vector<int>& arr, int target) {
+        int firstIndex = firstOccurance(arr, target);
+        int lastIndex = lastOccurance(arr, target);
+        
+        if(firstIndex == -1 && lastIndex == -1) {
+            return 0;
+        }
+        
+        int totalOccurance = (lastIndex - firstIndex) + 1;
+        
+        return totalOccurance;
+    }
+};
+
+
+
 //Brute Force
 class Solution {
   public:
