2ae5ea610fa7f342737949eeb686aa4ac6040002
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-01-17 15:22:04 +0530

    Create numberOfOccurances.cpp

---
+++ Binary Search/numberOfOccurances.cpp
@@ -0,0 +1,16 @@
+// Number of Occurances   ( GFG )
+
+//Brute Force
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        int count = 0;
+        
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
