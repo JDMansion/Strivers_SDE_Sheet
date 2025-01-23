f8e91846e9e5f6c5faa44ddbacbadffee029b67c
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-01-23 18:06:14 +0530

    Update searchInARotatedSortedArray.cpp

--- Binary Search/searchInARotatedSortedArray.cpp
+++ Binary Search/searchInARotatedSortedArray.cpp
@@ -63,3 +63,43 @@ public:
         return ans;
     }
 };
+
+
+
+
+//Striver's Approach:
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int start = 0;
+//         int end = nums.size() - 1;
+
+//         while(start <= end) {
+//             int mid = start + (end - start)/2;
+
+//             if(nums[mid] == target) {
+//                 return mid;
+//             }
+//             //left portion
+//             else if(nums[start] <= nums[mid]) {
+//                 if(nums[start] <= target && target <= nums[mid]) {
+//                     end = mid - 1;
+//                 }
+//                 else {
+//                     start = mid + 1;
+//                 }
+//             }
+//             //right portion
+//             else {
+//                 if(nums[mid] <= target && target <= nums[end]) {
+//                     start = mid + 1;
+//                 }
+//                 else {
+//                     end = mid - 1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
