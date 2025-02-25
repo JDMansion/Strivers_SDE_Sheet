5014003b72e804b38705b2260486a3b2ad04f6f6
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-02-25 20:55:29 +0530

    Update TwoSum.cpp

--- Arrays/Medium/TwoSum.cpp
+++ Arrays/Medium/TwoSum.cpp
@@ -1,5 +1,10 @@
 // Two Sum   ( Leetcode: 1 )
 
+// We can also use, unordered_map<> for this question. unordered_map is implemented using a hash map.
+//Use unordered_map when you need fast average-case lookup, insertion, and deletion and when the order of elements is not important.
+
+//map is typically implemented using a self-balancing binary search tree (usually a red-black tree).
+//Use map when you need elements to be stored in sorted order or when you need to perform operations that rely on the sorted order of keys.
 
 class Solution {
 public:
