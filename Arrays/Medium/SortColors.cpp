9cd61682a16fc7aafb52fa6bd6a870418098ff6d
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-02-26 23:29:03 +0530

    Create SortColors.cpp

---
+++ Arrays/Medium/SortColors.cpp
@@ -0,0 +1,24 @@
+// Sort Colors   ( Leetcode: 75 )
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int index = 0;
+
+        while(index <= right) {
+            if(nums[index] == 0) {
+                swap(nums[index], nums[left]);
+                index++, left++;
+            }
+            else if(nums[index] == 2) {
+                swap(nums[index], nums[right]);
+                right--;
+            }
+            else {
+                index++;
+            }
+        }
+    }
+};
