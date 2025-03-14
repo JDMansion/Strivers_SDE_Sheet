0e171234b01a39253216bb9a70bc207dc241e45d
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-03-14 23:58:47 +0530

    Create BestTimeToBuyAndSellAStock.cpp

---
+++ Arrays/Medium/BestTimeToBuyAndSellAStock.cpp
@@ -0,0 +1,16 @@
+// Best time to buy and Sell a stock ( Leetcode: 121 )
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minProfit = INT_MAX;
+
+        for(int i=0; i<prices.size(); i++) {
+            minProfit = min(minProfit, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minProfit);
+        }
+        return maxProfit;
+    }
+};
