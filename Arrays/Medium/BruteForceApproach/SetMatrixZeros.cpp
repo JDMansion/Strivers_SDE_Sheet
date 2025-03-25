0c548a952b7a60fd9d0c029635febb9daa941f46
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-03-25 17:51:18 +0530

    Update SetMatrixZeros.cpp

--- Arrays/Medium/BruteForceApproach/SetMatrixZeros.cpp
+++ Arrays/Medium/BruteForceApproach/SetMatrixZeros.cpp
@@ -1,9 +1,37 @@
-// Set Matrix Zeroes   ( Leetcode : 73 )
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
+//Better Approach
+vector<vector<int>> betterApproach(vector<vector<int>>& matrix) {
+    int r = matrix.size();
+    int c = matrix[0].size();
+
+    int rowArray[r] = {0};
+    int colArray[c] = {0};
+
+    //mark the row and col array indexes to 1 with respect to 0 present in the matrix
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            if(matrix[i][j] == 0) {
+                rowArray[i] = 1;
+                colArray[j] = 1;
+            }
+        }
+    }
+
+    //Now iterate through each index of the matrix, and for any value of either i or j being 1, replace that index with value 0
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            if(rowArray[i] || colArray[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+
+
+//Brute force 
 void setRow(vector<vector<int>>& matrix, int r, int c, int i) {
     //set all the element of the row to -1, except 0
     for(int j=0; j<c; j++) {
@@ -26,7 +54,6 @@ vector<vector<int>> zeroMatrix(vector<vector<int>>& matrix) {
     int r = matrix.size();
     int c= matrix[0].size();
 
-  //find where zeros are present, and replace the values of that row (i) and col (j) with -1 except the zeros
     for(int i=0; i<r; i++) {
         for(int j=0; j<c; j++) {
             if(matrix[i][j] == 0) {
@@ -36,7 +63,6 @@ vector<vector<int>> zeroMatrix(vector<vector<int>>& matrix) {
         }
     }
 
-  //finally replace all the -1 with value 0
     for(int i=0; i<r; i++) {
         for(int j=0; j<c; j++) {
             if(matrix[i][j] == -1) {
@@ -50,7 +76,12 @@ vector<vector<int>> zeroMatrix(vector<vector<int>>& matrix) {
 
 int main() {
     vector<vector<int>> matrix = { {1,1,1,0}, {1,0,1,1}, {1,1,1,1}};
-    vector<vector<int>> ans = zeroMatrix(matrix);
+    
+    //Brute Force
+    // vector<vector<int>> ans = zeroMatrix(matrix);
+
+    //Better Approach
+    vector<vector<int>> ans = betterApproach(matrix);
 
     for(int i=0; i<ans.size(); i++) {
         for(int j=0; j<ans[0].size(); j++) {
